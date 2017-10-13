8bc10b74bb83b07b7f12a190ed2e51f613a74cb8
sdoroshenko <sdoroshenko@corelogic.com>
2017-10-13 18:43:28 +0300

    fixed issue with arrays and strings with symbols comparison in quiz

---
+++ public/js/quiz.js
@@ -0,0 +1,71 @@
+var quizModule = (function () {
+	var currQuestion;
+	var display = "block";
+
+	function urlencodeFormData(fd){
+	    var s = '';
+	    function encode(s){ 
+	    	return encodeURIComponent(s).replace(/%20/g,'+'); 
+	    }
+	    for(var pair of fd.entries()){
+	        if(typeof pair[1]=='string'){
+	            s += (s?'&':'') + encode(pair[0])+'='+encode(pair[1]);
+	        }
+	    }
+	    return s;
+	}
+
+	return {
+		startQuiz: function(btn) {
+			var reqDataObj = {
+				method: "GET",
+				uri: "/quizstart",
+				action: "Get"
+			};
+			sendAjax(reqDataObj, function(res) {
+				btn.style.display = "none";
+				document.getElementById('squeezeContainer').style.display = "block";
+				console.log(res);
+
+				res.forEach (function(question){
+					var htmlQuests = "<div class='layer' style='display:"+display+"'><div>" + question.q + "</div><hr><ul>";
+					question.options.forEach(function(option, index) {
+						htmlQuests += '<li> <input type="'+question.tag+'" id="answ' + question.id + index + '" name="' + question.id + '" value="' + option + '" />';
+						htmlQuests += '<label class="radio-inline" for="answ' + question.id + index + '">' + option + '</li>';
+					});
+					htmlQuests += "</ul></div>"
+
+					document.getElementById('qContainer').innerHTML += htmlQuests;
+					display = "none";
+				});
+				currQuestion = document.getElementById("qContainer").firstChild;
+			});
+		},
+		nextQuestion: function(){
+			// console.log (currQuestion.nextElementSibling);
+			if (currQuestion.nextElementSibling != null) {
+				currQuestion.style.display = "none";
+				currQuestion = currQuestion.nextElementSibling;
+				currQuestion.style.display = "block";
+			} else {
+				document.querySelector('#squeezeContainer').style.display = "none";
+				// send answers
+				var inputTags = document.getElementById('qContainer').querySelectorAll('input:checked');
+				var formData = new FormData();
+				for (var i = 0; i < inputTags.length; i++) {
+					console.log(inputTags[i].getAttribute("name") + " : " + inputTags[i].getAttribute("value"));
+					formData.append(inputTags[i].getAttribute("name"), inputTags[i].getAttribute("value"));
+				}
+				var xhr = new XMLHttpRequest;
+				xhr.open('POST', '/getAnswers', true);
+				xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				xhr.send(urlencodeFormData(formData));
+				// output results
+				xhr.onreadystatechange = function() {
+					if (xhr.readyState != 4) return;
+					document.getElementById("quizArea").innerHTML = '<div class="male fontSize2"> Your result is: ' + JSON.parse(xhr.responseText).score + ' % </div>';
+				}
+			}
+		}
+	};
+})();
---
+++ router/quests.js
@@ -0,0 +1,70 @@
+(function() {
+	var counter = 10;
+	var users = [];
+	var questions = [{
+			id: "a1",
+			q: "Which tag inserts an image to HTML document?",
+			options: ["image", "img", "picture"],
+			tag: "radio"
+		},
+		{
+			id: "a2",
+			q: "Каким тегом можно создать нумерованый список",
+			options: ["list", "dl", "ul", "ol"],
+			tag: "radio"
+		},
+		{
+			id: "a3",
+			q: "Types for input tags",
+			options: ["text", "password", "image", "button"],
+			tag: "checkbox"
+		},
+		{
+			id: "a4",
+			q: "Выберите правильный вариант создания чекбокса",
+			options: ["&lt;input type=&quot;checkbox&quot;&gt;", "&lt;input type=&quot;check&quot;", "&lt;check&gt;", "&lt;checkbox&gt;"],
+			tag: "radio"
+		},
+		{
+			id: "a5",
+			q: "Выберите правильный вариант создания комментариев в HTML",
+			options: ["&lt;!-- ... --&gt;", "/* ... */", "// .."],
+			tag: "radio"
+		},
+		{
+			id: "a6",
+			q: "Что обозначает аббревиатура HTML",
+			options: ["Hyper Text Markup Language", "Hot Mail", "How to Make Lasagna"],
+			tag: "radio"
+		}
+	];
+	var correctAnswers = new Map();
+	correctAnswers.set("a1", "img");
+	correctAnswers.set("a2", "ol");
+	correctAnswers.set("a3", ["text", "password", "button"]);
+	correctAnswers.set("a4", '<input type="checkbox">');
+	correctAnswers.set("a5", '<!-- ... -->');
+	correctAnswers.set("a6", "Hyper Text Markup Language");
+
+	module.exports = {
+		getQuestions: function(cb) {
+			cb(questions);
+		},
+		checkAnswers: function(quizResults, cb) {
+			var rightCounter = 0;
+ 			for (key in quizResults) {
+ 				if (( quizResults[key] == correctAnswers.get(key)) && (quizResults[key] != 'object')) {
+ 					rightCounter++;
+ 				} else if ( quizResults[key].join('') == correctAnswers.get(key).join('') ) {
+					rightCounter++;
+ 				} else {
+ 					console.log (quizResults[key] + " : " + correctAnswers.get(key));
+ 					console.log (typeof quizResults[key] );
+ 				}
+ 			}
+ 			console.log (rightCounter);
+ 			console.log (Object.keys(quizResults).length);
+ 			cb(Math.round(rightCounter * 100 / Object.keys(quizResults).length));
+		}
+	};
+}());
