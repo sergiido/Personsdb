fc1a94d8803778ca3dfce0e752d78ddcd4d27146
sergiido <serpontin@bigmir.net>
2018-03-13 18:02:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const low = require('lowdb');
 const app = express();
 
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8888;
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.disable('x-powered-by');
